62440f6dff728608db62ae8ff1378e5c5cf28772
Mr. Pine <50425705+Mr-Pine@users.noreply.github.com>
2021-09-30 00:00:02 +0200

    Sachen gemacht

--- app/src/main/java/de/mrpine/xkcdfeed/MainActivity.kt
+++ app/src/main/java/de/mrpine/xkcdfeed/MainActivity.kt
@@ -3,21 +3,101 @@ package de.mrpine.xkcdfeed
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Share
+import androidx.compose.material.icons.filled.Star
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.lifecycle.ViewModel
+import de.mrpine.xkcdfeed.ui.theme.Shapes
 import de.mrpine.xkcdfeed.ui.theme.XKCDFeedTheme
+import kotlinx.coroutines.launch
 
+private const val TAG = "MainActivity"
+
+@ExperimentalMaterialApi
 class MainActivity : ComponentActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            XKCDFeedTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(color = MaterialTheme.colors.background) {
-                    Greeting("Android")
+            MainContent()
+        }
+    }
+}
+
+@ExperimentalMaterialApi
+@Composable
+fun MainContent(mainViewModel: MainViewModel = MainViewModel()) {
+    val favSheetState = rememberModalBottomSheetState(initialValue = ModalBottomSheetValue.Hidden)
+    val scope = rememberCoroutineScope();
+
+    fun showFavSheet() {
+        scope.launch { favSheetState.show() }
+    }
+
+    XKCDFeedTheme {
+        Surface {
+            SheetLayout(favSheetState, ::showFavSheet)
+        }
+    }
+}
+
+@ExperimentalMaterialApi
+@Composable
+fun MainScaffold(showFavSheet: () -> Unit) {
+
+    Scaffold(floatingActionButton = {
+        FloatingActionButton(onClick = showFavSheet) {
+            Icon(Icons.Default.Star, "favourite")
+        }
+    }) {
+        Text(text = "hallöchen")
+    }
+}
+
+@ExperimentalMaterialApi
+@Composable
+fun SheetLayout(state: ModalBottomSheetState, showFavSheet: () -> Unit) {
+    ModalBottomSheetLayout(
+        sheetContent = sheetContent(),
+        sheetShape = RoundedCornerShape(topStart = 12.dp, topEnd = 12.dp),
+        sheetState = state
+    ) {
+        MainScaffold(showFavSheet)
+    }
+}
+
+@Composable
+fun sheetContent(): @Composable() (ColumnScope.() -> Unit) {
+    return {
+        Surface(modifier = Modifier.padding(8.dp)) {
+            Column {
+                Row(verticalAlignment = Alignment.CenterVertically) {
+                    Surface(modifier = Modifier
+                        .width(30.dp)
+                        .height(30.dp)
+                        .padding(end = 6.dp)) {
+                        Icon(Icons.Default.Star, "Star")
+                    }
+                    Text("Fav")
+                }
+                Divider(modifier = Modifier.padding(vertical = 4.dp))
+                Row(verticalAlignment = Alignment.CenterVertically) {
+                    Surface(modifier = Modifier
+                        .width(30.dp)
+                        .height(30.dp)
+                        .padding(end = 6.dp)) {
+                        Icon(Icons.Default.Share, "Share")
+                    }
+                    Text(text = "Share")
                 }
             }
         }
@@ -29,10 +109,24 @@ fun Greeting(name: String) {
     Text(text = "Hello $name!")
 }
 
-@Preview(showBackground = true)
+@ExperimentalMaterialApi
+@Preview(showBackground = true, name = "Main Preview")
 @Composable
 fun DefaultPreview() {
-    XKCDFeedTheme {
-        Greeting("Android")
+    XKCDFeedTheme(darkTheme = true) {
+        MainContent()
     }
+}
+
+@ExperimentalMaterialApi
+@Preview(name = "Sheet Preview", showBackground = true)
+@Composable
+fun SheetPreview() {
+    XKCDFeedTheme {
+        Column(content = sheetContent())
+    }
+}
+
+@ExperimentalMaterialApi
+class MainViewModel : ViewModel() {
 }
