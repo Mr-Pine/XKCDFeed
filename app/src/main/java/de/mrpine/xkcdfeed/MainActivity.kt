36481dedff74d2e2239537a8eb7c3a9fdb8020e5
Mr. Pine <50425705+Mr-Pine@users.noreply.github.com>
2021-09-30 22:54:25 +0200

    Ganz schön viele Sachen gemacht

--- app/src/main/java/de/mrpine/xkcdfeed/MainActivity.kt
+++ app/src/main/java/de/mrpine/xkcdfeed/MainActivity.kt
@@ -9,33 +9,55 @@ import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Share
 import androidx.compose.material.icons.filled.Star
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.*
+import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
-import de.mrpine.xkcdfeed.ui.theme.Shapes
+import androidx.lifecycle.viewmodel.compose.viewModel
+import androidx.navigation.NavController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.google.accompanist.pager.ExperimentalPagerApi
+import com.google.accompanist.pager.HorizontalPager
+import com.google.accompanist.pager.pagerTabIndicatorOffset
+import com.google.accompanist.pager.rememberPagerState
+import com.google.accompanist.systemuicontroller.rememberSystemUiController
 import de.mrpine.xkcdfeed.ui.theme.XKCDFeedTheme
 import kotlinx.coroutines.launch
 
 private const val TAG = "MainActivity"
 
+@ExperimentalPagerApi
 @ExperimentalMaterialApi
 class MainActivity : ComponentActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         setContent {
-            MainContent()
+            val navController = rememberNavController()
+
+            XKCDFeedTheme() {
+                NavHost(navController = navController, startDestination = "mainView") {
+                    composable("mainView") { mainContent() }
+                    composable("test") { Text("hello") }
+                }
+            }
         }
     }
 }
 
+@ExperimentalPagerApi
 @ExperimentalMaterialApi
 @Composable
-fun MainContent(mainViewModel: MainViewModel = MainViewModel()) {
+fun mainContent(mainViewModel: MainViewModel = viewModel()) {
     val favSheetState = rememberModalBottomSheetState(initialValue = ModalBottomSheetValue.Hidden)
     val scope = rememberCoroutineScope();
 
@@ -43,35 +65,84 @@ fun MainContent(mainViewModel: MainViewModel = MainViewModel()) {
         scope.launch { favSheetState.show() }
     }
 
-    XKCDFeedTheme {
-        Surface {
-            SheetLayout(favSheetState, ::showFavSheet)
-        }
-    }
+    val tabIndex: Int by mainViewModel.tabIndex.observeAsState(1)
+
+    sheetLayout(
+        favSheetState,
+        ::showFavSheet
+    )
 }
 
+@ExperimentalPagerApi
 @ExperimentalMaterialApi
 @Composable
-fun MainScaffold(showFavSheet: () -> Unit) {
+fun mainScaffold(showFavSheet: () -> Unit) {
+
+    val tabPagerState = rememberPagerState(0)
+    val scope = rememberCoroutineScope()
 
     Scaffold(floatingActionButton = {
         FloatingActionButton(onClick = showFavSheet) {
-            Icon(Icons.Default.Star, "favourite")
+            Icon(
+                Icons.Default.Star,
+                "favourite"
+            )
         }
-    }) {
-        Text(text = "hallöchen")
+    }, topBar = { topAppBar() }) {
+        Column {
+            TabRow(
+                selectedTabIndex = tabPagerState.currentPage,
+                indicator = { tabPositions ->
+                    TabRowDefaults.Indicator(
+                        Modifier.pagerTabIndicatorOffset(tabPagerState, tabPositions)
+                    )
+                }
+            ) {
+                Tab(
+                    text = { Text("lol 1") },
+                    selected = tabPagerState.currentPage == 0,
+                    onClick = { scope.launch { tabPagerState.animateScrollToPage(0) } },
+                    selectedContentColor = Color(0xFFFFFFFF),
+                    unselectedContentColor = Color(0x80FFFFFF)
+                )
+                Tab(
+                    text = { Text("lol 2") },
+                    selected = tabPagerState.currentPage == 1,
+                    onClick = { scope.launch { tabPagerState.animateScrollToPage(1) } },
+                    selectedContentColor = Color(0xFFFFFFFF),
+                    unselectedContentColor = Color(0x80FFFFFF)
+                )
+            }
+            HorizontalPager(count = 2, state = tabPagerState) { page ->
+                Text("lol Seite $page")
+            }
+        }
     }
 }
 
+@Composable
+fun topAppBar() {
+    return TopAppBar(
+        title = { Text(text = "XKCDFeed") },
+        contentColor = Color.White,
+        elevation = 0.dp
+    )
+}
+
+@ExperimentalPagerApi
 @ExperimentalMaterialApi
 @Composable
-fun SheetLayout(state: ModalBottomSheetState, showFavSheet: () -> Unit) {
+fun sheetLayout(
+    state: ModalBottomSheetState,
+    showFavSheet: () -> Unit
+) {
     ModalBottomSheetLayout(
         sheetContent = sheetContent(),
         sheetShape = RoundedCornerShape(topStart = 12.dp, topEnd = 12.dp),
-        sheetState = state
+        sheetState = state,
+        scrimColor = MaterialTheme.colors.primaryVariant.copy(alpha = 0.7f)
     ) {
-        MainScaffold(showFavSheet)
+        mainScaffold(showFavSheet)
     }
 }
 
@@ -80,21 +151,26 @@ fun sheetContent(): @Composable() (ColumnScope.() -> Unit) {
     return {
         Surface(modifier = Modifier.padding(8.dp)) {
             Column {
+                val iconDimensions = 30.dp
                 Row(verticalAlignment = Alignment.CenterVertically) {
-                    Surface(modifier = Modifier
-                        .width(30.dp)
-                        .height(30.dp)
-                        .padding(end = 6.dp)) {
+                    Surface(
+                        modifier = Modifier
+                            .width(iconDimensions)
+                            .height(iconDimensions)
+                            .padding(end = 6.dp)
+                    ) {
                         Icon(Icons.Default.Star, "Star")
                     }
                     Text("Fav")
                 }
                 Divider(modifier = Modifier.padding(vertical = 4.dp))
                 Row(verticalAlignment = Alignment.CenterVertically) {
-                    Surface(modifier = Modifier
-                        .width(30.dp)
-                        .height(30.dp)
-                        .padding(end = 6.dp)) {
+                    Surface(
+                        modifier = Modifier
+                            .width(iconDimensions)
+                            .height(iconDimensions)
+                            .padding(end = 6.dp)
+                    ) {
                         Icon(Icons.Default.Share, "Share")
                     }
                     Text(text = "Share")
@@ -104,17 +180,13 @@ fun sheetContent(): @Composable() (ColumnScope.() -> Unit) {
     }
 }
 
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
+@ExperimentalPagerApi
 @ExperimentalMaterialApi
 @Preview(showBackground = true, name = "Main Preview")
 @Composable
 fun DefaultPreview() {
-    XKCDFeedTheme(darkTheme = true) {
-        MainContent()
+    XKCDFeedTheme(darkTheme = false) {
+        mainContent()
     }
 }
 
@@ -122,11 +194,17 @@ fun DefaultPreview() {
 @Preview(name = "Sheet Preview", showBackground = true)
 @Composable
 fun SheetPreview() {
-    XKCDFeedTheme {
+    XKCDFeedTheme(darkTheme = false) {
         Column(content = sheetContent())
     }
 }
 
 @ExperimentalMaterialApi
 class MainViewModel : ViewModel() {
+    private val _tabIndex = MutableLiveData(0)
+    val tabIndex: LiveData<Int> = _tabIndex
+
+    fun setTabIndex(index: Int) {
+        _tabIndex.value = index
+    }
 }
--- app/src/main/java/de/mrpine/xkcdfeed/ui/theme/Color.kt
+++ app/src/main/java/de/mrpine/xkcdfeed/ui/theme/Color.kt
@@ -6,3 +6,15 @@ val Purple200 = Color(0xFFBB86FC)
 val Purple500 = Color(0xFF6200EE)
 val Purple700 = Color(0xFF3700B3)
 val Teal200 = Color(0xFF03DAC5)
+
+val Gray900 = Color(0xFF212121)
+val Gray700 = Color(0xFF616161)
+val Gray500 = Color(0xFF939393)
+val Gray400 = Color(0xFFbdbdbd)
+
+val Gray400Light = Color(0xFFefefef)
+val Gray400Dark = Color(0xFF8d8d8d)
+val Gray500Light = Color(0xFFcfcfcf)
+val Gray500Dark = Color(0xFF707070)
+val Gray900Light = Color(0xFF212121)
+val Gray900Dark = Color.Black
