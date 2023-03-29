b8645d3feb3008d2405ec9bee9777b38635a13f7
Mr. Pine <git@mr-pine.de>
2023-03-29 22:57:53 +0200

    Transition to version catalog

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,4 +1,3 @@
-
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 import java.io.FileInputStream
 import java.util.*
@@ -18,7 +17,7 @@ android {
     signingConfigs {
         create("release") {
             storeFile = file(signingProperties["signing.release.storeFile"] as String)
-            keyAlias  = signingProperties["signing.release.keyAlias"] as String
+            keyAlias = signingProperties["signing.release.keyAlias"] as String
             storePassword = signingProperties["signing.release.password"] as String
             keyPassword = signingProperties["signing.release.password"] as String
         }
@@ -69,37 +68,26 @@ android {
 }
 
 dependencies {
-
-    val compose_version = libs.versions.compose.get()
-
     implementation(libs.material)
     implementation(libs.bundles.compose)
-
-
     implementation(libs.bundles.androidx)
+    implementation(libs.bundles.accompanist)
 
     implementation(platform(libs.firebase.bom))
     implementation(libs.bundles.firebase)
 
-    implementation("com.google.android.gms:play-services-auth:20.3.0")
+    implementation(libs.playAuth)
+
+    implementation(libs.ossLicences)
+    implementation(libs.volley)
+    implementation(libs.coil)
+    implementation(libs.zoomables)
+
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.3")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.4.0")
-    androidTestImplementation("androidx.compose.ui:ui-test-junit4:$compose_version")
-    debugImplementation("androidx.compose.ui:ui-tooling:$compose_version")
+    androidTestImplementation("androidx.compose.ui:ui-test-junit4:${libs.versions.compose.get()}")
 
-    implementation(libs.bundles.accompanist)
-
-    implementation("androidx.navigation:navigation-compose:2.5.1")
-    implementation("androidx.compose.runtime:runtime-livedata:$compose_version")
-    implementation("com.android.volley:volley:1.2.1")
-    implementation("androidx.datastore:datastore-preferences:1.0.0")
-
-    implementation("com.google.android.gms:play-services-oss-licenses:17.0.0")
-
-    implementation("io.coil-kt:coil-compose:2.2.0")
-
-    implementation("de.mr-pine.utils:zoomables:1.1.1")
 }
 
 tasks.withType<KotlinCompile> {
--- build.gradle.kts
+++ build.gradle.kts
@@ -8,7 +8,7 @@ buildscript {
         classpath("com.android.tools.build:gradle:7.4.2")
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:${libs.versions.kotlin.get()}")
         classpath("com.google.gms:google-services:4.3.15")
-        classpath("com.google.android.gms:oss-licenses-plugin:0.10.6")
+        classpath("com.google.android.gms:oss-licenses-plugin:${libs.versions.ossLicences.plugin.get()}")
         // NOTE: Do not place your application dependencies here; they belong
         // in the individual module build.gradle files
     }
